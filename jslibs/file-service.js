69e636e5caa896f870d02d85adb184870844cc40
Aeron <raymana@3c22fbe44412.ant.amazon.com>
2023-09-07 10:16:36 -0700

    create folders in temp location

--- jslibs/file-service.js
+++ jslibs/file-service.js
@@ -1,37 +1,48 @@
 const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
 const jsonstorepath = "/json-data/";
 
 module.exports = function (app) {
     app.get('/jsonstore', (req, res) => {
-        createFolderIfNotExist(jsonstorepath);
-        const fileName = jsonstorepath + req.query.filename;
+        createFolderIfNotExist(getTempFolderPath());
+        const fileName = getTempFolderPath() + req.query.filename;
         fs.readFile(fileName, 'utf8', (err, data) => {
             if (err) {
-                res.status(500).send({'data': err.message});
+                res.status(500).send({ 'data': err.message });
+            } else {
+                res.status(200).send({ 'data': data });
             }
-            res.status(200).send({'data': data});
         })
     })
 
     app.post('/jsonstore', (req, res) => {
-        createFolderIfNotExist(jsonstorepath);
-        const fileName = jsonstorepath + req.query.filename;
+        createFolderIfNotExist(getTempFolderPath());
+        const fileName = getTempFolderPath() + req.query.filename;
         const content = req.body.content.data;
 
-        fs.writeFile(fileName, JSON.stringify(content), 'utf8', (err)=>{
+        fs.writeFile(fileName, JSON.stringify(content), 'utf8', (err) => {
             if (err) {
-                res.status(500).send({'data': err.message});
+                res.status(500).send({ 'data': err.message });
             } else {
-                res.status(200).send({'data': 'File written : ' + fileName});
+                res.status(200).send({ 'data': 'File written : ' + fileName });
             }
         })
     })
 
+    function getTempFolderPath() {
+        const tempDir = os.tmpdir();
+        return tempDir + jsonstorepath;
+    }
+
     function createFolderIfNotExist(folderPath) {
         if (!fs.existsSync(folderPath)) {
-            fs.mkdir(folderPath, (err) => {
-                if (err) throw err;
-            })
+            fs.mkdir(folderPath, { recursive: true }), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            }
         }
     }
 }
