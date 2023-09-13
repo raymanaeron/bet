89acddb4689cb7b03f17405b660ebd12992c1116
raymanaeron <raymana@live.com>
2023-09-13 16:18:40 -0700

    incorporated date parameters in the external service calls

--- jslibs/odds-api-service.js
+++ jslibs/odds-api-service.js
@@ -3,10 +3,18 @@ const https = require('https');
 module.exports = function (app) {
 
     app.get('/proentire', (req, res) => {
+        const api_key = "86b17191d64318c100afd23799071269";
+        const currentDateUTC = new Date();
+        const futureDateUTC = new Date(currentDateUTC);
+        futureDateUTC.setHours(currentDateUTC.getHours() + 72);
         
-        var api_key = "86b17191d64318c100afd23799071269";
-        var entiregameurl = 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=' + api_key + '&regions=us&markets=h2h,spreads,totals&oddsFormat=american&bookmakers=draftkings,betmgm,williamhill_us,fanduel';
-        
+        var fromDateParam = '&commenceTimeFrom=' + currentDateUTC.toISOString().slice(0, -5) + 'Z';;
+        var toDateParam = '&commenceTimeTo='+ futureDateUTC.toISOString().slice(0, -5) + 'Z';;
+        var entiregameurl = 'https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=' 
+            + api_key 
+            + '&regions=us&markets=h2h,spreads,totals&oddsFormat=american&bookmakers=draftkings,betmgm,williamhill_us,fanduel'
+            + fromDateParam + toDateParam;
+
         https.get(entiregameurl, (httpresponse) => {
             let data = '';
 
