c3418bbbc58bbec02f530e4ce7d7da0a03574397
raymanaeron <raymana@live.com>
2023-09-26 20:14:02 -0700

    cleanup

--- publocal.bat
+++
@@ -1,5 +0,0 @@
-copy ..\bet-app-out\.env ..\bet-app-out\.env_old
-xcopy *.* /v /y ..\bet-app-out\
-xcopy /s /v /y jslibs\*.* ..\bet-app-out\jslibs\
-xcopy /s /v /y app-ux\amznkey ..\bet-app-out\app-ux\amznkey\
-xcopy /s /v /y app-ux\dist ..\bet-app-out\app-ux\dist\
